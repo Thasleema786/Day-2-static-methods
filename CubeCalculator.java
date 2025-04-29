09b314eec75a0d0a6e0c3daa304857b679d31bb1
Thasleema786 <thasleemabegumshaik786@gmail.com>
2025-04-29 18:55:49 +0530

    Add files via upload

---
+++ CubeCalculator.java
@@ -0,0 +1,8 @@
+public class CubeCalculator{
+public static void cubeOfNumbers(int n){
+	System.out.println("Cube:"+(n*n*n));
+	}
+public static void main(String[] args){
+ 	 cubeOfNumbers(3);
+}
+}
---
+++ SquareCalculator.java
@@ -0,0 +1,8 @@
+public class SquareCalculator{
+public static void squareOfNumber(int n){
+	System.out.println("Square:"+(n*n));
+	}
+public static void main(String[] args){
+	 squareOfNumber(2);
+}
+}
